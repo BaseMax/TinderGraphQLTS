940b02c1c35700ddfeaa799c22e92eb8fd794fcf
khalvai <khalvayozbek@gmail.com>
2023-07-15 12:06:50 +0330

    setup project

---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { join } from "path";
+import { GraphQLError, GraphQLFormattedError } from "graphql";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      formatError: (error: GraphQLError) => {
+        const graphQLFormattedError: GraphQLFormattedError = {
+          message:
+            (error?.extensions?.exception as any)?.response?.message ||
+            error?.message,
+        };
+        return graphQLFormattedError;
+      },
+    }),
+
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow("DATABASE_URI"),
+      }),
+
+      inject: [ConfigService],
+    }),
+
+    AuthModule,
+  ],
+  providers: [],
+})
+export class AppModule {}
