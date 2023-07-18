d097edd1bd854167384152109e1050dd84f44fe3
khalvai <khalvayozbek@gmail.com>
2023-07-18 15:35:38 +0330

    add : error handler

--- src/expection/error.handling.ts
+++ src/expection/error.handling.ts
@@ -1,10 +1,12 @@
 import {
   ArgumentsHost,
   Catch,
+  ConflictException,
   HttpException,
   HttpStatus,
 } from "@nestjs/common";
 import { GqlArgumentsHost, GqlExceptionFilter } from "@nestjs/graphql";
+import { MongooseError } from "mongoose";
 
 @Catch()
 export class GraphqlErrorFilter implements GqlExceptionFilter {
@@ -12,11 +14,27 @@ export class GraphqlErrorFilter implements GqlExceptionFilter {
     const gqlHost = GqlArgumentsHost.create(host);
     const ctx = gqlHost.getContext();
 
+    const inferredDatabaseError = this.inferDatabaseError(exception);
+
     const errorResponse = {
-      message: exception.message || "Internal server error",
+      message:
+        inferredDatabaseError?.message ||
+        exception.message ||
+        "Internal server error",
     };
 
-    const status = exception.status || HttpStatus.INTERNAL_SERVER_ERROR;
+    const status =
+      inferredDatabaseError?.status ||
+      exception.status ||
+      HttpStatus.INTERNAL_SERVER_ERROR;
     throw new HttpException(errorResponse.message, status);
+  }
+
+  inferDatabaseError(exception: any) {
+    if (exception.code === 11000) {
+      return { message: "you are already in a match", status: 409 }; // Use NestJS's built-in ConflictException to handle 409 conflict status
+    }
+
+    return null;
   }
 }
